0d2df75724cb5e3cbc6862a30eedcfb282af4bef
Subhankar Ghosh <121926051+subhankar2524@users.noreply.github.com>
2024-09-11 00:29:06 +0530

    connect to mongo atas

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+/*
+import express from "express"
+const app = express()
+
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("Error", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+})()
+
+*/
